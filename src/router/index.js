ac3fe1ac0b0530a9ba070e09577177a109556d65
mugishap <precieuxmugisha@gmail.com>
2023-02-01 12:04:05 +0200

    [init] Project setup

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from "vue-router"
+import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+import Dashboard from '../views/Home/Dashboard.vue'
+import NewSurvey from '../views/survey/NewSurvey.vue'
+import Survey from '../views/survey/Survey.vue'
+
+const routes = [
+    {
+        path: "/auth/login",
+        name: "Login",
+        component: Login
+    },
+    {
+        path: "/auth/signup",
+        name: "Signup",
+        component: Signup
+    },
+    {
+        path: "/survey/new",
+        name: "NewSurvey",
+        component: NewSurvey
+    },
+    {
+        path: "/survey/{surveyId}",
+        name: "Survey",
+        component: Survey
+    },
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: Dashboard
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default { router }
---
+++ src/views/auth/Signup.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Signup</div>
+</template>
+<script lang="js">
+export default {
+  name: "Signup",
+};
+</script>
+<style></style>
